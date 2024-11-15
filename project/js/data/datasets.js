7483a71e9f06982bea9c58d25772c2382626fa32
Simón Pedro <simon.pedro.g@gmail.com>
2024-11-14 19:22:01 -0700

    get satellite data

--- project/js/data/datasets.js
+++ project/js/data/datasets.js
@@ -1,11 +1,13 @@
 import { getPlanetsData, getOldPlanetsData } from "./planets.js";
 import { getMissionsData, getMissionSimplePathData } from "./missions.js";
+import { getSatellitesData } from "./satellites.js";
 
 
 export {
     getPlanetsData,
     getOldPlanetsData,
     getMissionsData,
-    getMissionSimplePathData
+    getMissionSimplePathData,
+    getSatellitesData
 }
 
