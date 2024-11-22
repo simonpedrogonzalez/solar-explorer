c587e473dd54517ff288f00feb75f728c3b4f260
Matthew Whitaker <matthew.w3000@gmail.com>
2024-11-21 23:09:54 -0700

    Connect scatter plot code

--- project/js/vis/scatter-hist-dashboard/bodySection.js
+++ project/js/vis/scatter-hist-dashboard/bodySection.js
@@ -1,24 +1,25 @@
 import { populateSelect } from "./utils.js";
 import { getBodiesData } from "../../data/bodies.js";
 import * as histogram from "./histogram.js";
+import * as scatterPlot from "./scatterPlot.js";
 
 export const setup = async () => {
 
     const bodiesData = await getBodiesData();
 
     const bodiesVariables = [
-        { value: "radius", text: "Radius (km)" },
+        { value: "radius", text: "Radius (km)", scale: "log" },
         // { value: "mass_value", text: "Mass Value (10^x kg)" },
         // { value: "mass_exponent", text: "Mass Exponent (kg)" },
         // { value: "vol_value", text: "Volume Value (10^x km³)" },
         // { value: "vol_exponent", text: "Volume Exponent (km³)" },
-        { value: "density", text: "Density (g/cm³)" },
-        { value: "gravity", text: "Gravity (m/s²)" },
-        { value: "sideral_orbit", text: "Sideral Orbit (days)" },
-        { value: "sideral_rotation", text: "Sideral Rotation (hours)" },
-        { value: "discovered_by", text: "Discovered By" },
-        { value: "discovery_date", text: "Discovery Date" },
-        { value: "avg_temp_kelvin", text: "Average Temperature (K)" }
+        { value: "density", text: "Density (g/cm³)", scale: "linear" },
+        { value: "gravity", text: "Gravity (m/s²)", scale: "linear" },
+        { value: "sideral_orbit", text: "Sideral Orbit (days)", scale: "log" },
+        { value: "sideral_rotation", text: "Sideral Rotation (hours)", scale: "linear" },
+        // { value: "discovered_by", text: "Discovered By" },
+        { value: "discovery_date", text: "Discovery Date", scale: "time" },
+        { value: "avg_temp_kelvin", text: "Average Temperature (K)", scale: "linear" },
     ];
 
     const histogramContainer = document.getElementById("bodies-hist");
@@ -40,11 +41,38 @@ export const setup = async () => {
         
     });
 
+    bodiesHistogramVariableSelector.dispatchEvent(new Event('change'));
+
     bodiesScatterSelectors.x.addEventListener("change", () => {
         console.log(bodiesScatterSelectors.x.value);
+        scatterPlot.draw(
+            scatterContainer,
+            bodiesData,
+            bodiesScatterSelectors.x.value,
+            bodiesScatterSelectors.y.value,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.x.value).text,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.y.value).text,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.x.value).scale,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.y.value).scale
+        );
     });
 
     bodiesScatterSelectors.y.addEventListener("change", () => {
         console.log(bodiesScatterSelectors.y.value);
+        scatterPlot.draw(
+            scatterContainer,
+            bodiesData,
+            bodiesScatterSelectors.x.value,
+            bodiesScatterSelectors.y.value,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.x.value).text,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.y.value).text,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.x.value).scale,
+            bodiesVariables.find((v) => v.value === bodiesScatterSelectors.y.value).scale
+        );
     });
+
+    bodiesScatterSelectors.x.value = "radius";
+    bodiesScatterSelectors.y.value = "sideral_orbit";
+
+    bodiesScatterSelectors.x.dispatchEvent(new Event('change'));
 }
---
+++ project/js/vis/scatter-hist-dashboard/missionSection.js
@@ -0,0 +1,70 @@
+import { populateSelect } from "./utils.js";
+import { getMissionsData } from "../../data/missions.js";
+import * as histogram from "./histogram.js";
+import * as scatterPlot from "./scatterPlot.js";
+
+export const setup = async () => {
+
+    const missionData = await getMissionsData();
+
+    const missionVariables = [
+        { value: "launch_date", text: "Launch Date", scale: "time" },
+        { value: "total_mass", text: "Total Mass (kg)", scale: "log" },
+        { value: "num_pieces", text: "# Spacecraft Pieces", scale: "linear" },
+        { value: "duration", text: "Duration (days)", scale: "log" },
+    ];
+
+    const histogramContainer = document.getElementById("mission-hist");
+    const scatterContainer = document.getElementById("mission-scatter");
+
+    const missionHistogramVariableSelector = document.getElementById("hist-mission");
+    const missionScatterSelectors = {
+        x: document.getElementById("x-mission"),
+        y: document.getElementById("y-mission")
+    };
+
+    populateSelect(missionHistogramVariableSelector, missionVariables);
+    populateSelect(missionScatterSelectors.x, missionVariables);
+    populateSelect(missionScatterSelectors.y, missionVariables);
+
+    missionHistogramVariableSelector.addEventListener("change", () => {
+        console.log(missionHistogramVariableSelector.value);
+        histogram.draw(histogramContainer, missionData, missionHistogramVariableSelector.value);
+
+    });
+
+    missionHistogramVariableSelector.dispatchEvent(new Event('change'));
+
+    missionScatterSelectors.x.addEventListener("change", () => {
+        console.log(missionScatterSelectors.x.value);
+        scatterPlot.draw(
+            scatterContainer,
+            missionData,
+            missionScatterSelectors.x.value,
+            missionScatterSelectors.y.value,
+            missionVariables.find((v) => v.value === missionScatterSelectors.x.value).text,
+            missionVariables.find((v) => v.value === missionScatterSelectors.y.value).text,
+            missionVariables.find((v) => v.value === missionScatterSelectors.x.value).scale,
+            missionVariables.find((v) => v.value === missionScatterSelectors.y.value).scale
+        );
+    });
+
+    missionScatterSelectors.y.addEventListener("change", () => {
+        console.log(missionScatterSelectors.y.value);
+        scatterPlot.draw(
+            scatterContainer,
+            missionData,
+            missionScatterSelectors.x.value,
+            missionScatterSelectors.y.value,
+            missionVariables.find((v) => v.value === missionScatterSelectors.x.value).text,
+            missionVariables.find((v) => v.value === missionScatterSelectors.y.value).text,
+            missionVariables.find((v) => v.value === missionScatterSelectors.x.value).scale,
+            missionVariables.find((v) => v.value === missionScatterSelectors.y.value).scale
+        );
+    });
+
+    missionScatterSelectors.x.value = "launch_date";
+    missionScatterSelectors.y.value = "duration";
+
+    missionScatterSelectors.x.dispatchEvent(new Event('change'));
+}
